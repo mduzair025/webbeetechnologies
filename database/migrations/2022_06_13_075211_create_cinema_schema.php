4fae3a8b1dcca6295988ea9f3661819aa19f984b
mduzair025 <mduzair025@gmail.com>
2023-05-26 18:25:56 +0500

    4th task has been completed

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -36,7 +36,100 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        Schema::create('cinemas', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('city');
+            $table->integer('no_of_rooms');
+            $table->timestamps();
+        });
+        
+        Schema::create('rooms', function($table) {
+            $table->increments('id');
+            $table->integer('cinema_id')->unsigned();
+            $table->foreign('cinema_id')->references('id')->on('cinemas')->onDelete('cascade');
+            $table->string('name');
+            $table->integer('no_of_seats');
+            $table->timestamps();
+        });
+
+        Schema::create('movies', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('duration');
+            $table->integer('rating');
+            $table->string('genre');
+            $table->integer('status');
+            $table->timestamps();
+        });
+
+        Schema::create('shows', function($table) {
+            $table->increments('id');
+            $table->integer('movie_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->time('time');
+            $table->date('date');
+            $table->timestamps();
+        });
+
+        Schema::create('rooms_shows_mapper', function($table) {
+            $table->increments('id');
+            $table->integer('room_id')->unsigned();
+            $table->integer('show_id')->unsigned();
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+            $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('seat_types', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('discount');
+            $table->timestamps();
+        });
+
+        Schema::create('seats', function($table) {
+            $table->increments('id');
+            $table->integer('room_id')->unsigned();
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+            $table->integer('seat_type_id')->unsigned();
+            $table->foreign('seat_type_id')->references('id')->on('seat_types')->onDelete('cascade');
+            $table->integer('row');
+            $table->integer('number');
+            $table->timestamps();
+        });
+
+        Schema::create('fares', function($table) {
+            $table->increments('id');
+            $table->integer('show_id')->unsigned();
+            $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+            $table->integer('seat_id')->unsigned();
+            $table->foreign('seat_id')->references('id')->on('seats')->onDelete('cascade');
+            $table->double('price');
+            $table->timestamps();
+        });
+        
+        Schema::create('bookings', function($table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('room_id')->unsigned();
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+            $table->double('price');
+            $table->string('code');
+            $table->integer('status');
+            $table->timestamps();
+        });
+        
+        Schema::create('bookings_seats_mapper', function($table){
+            $table->increments('id');
+            $table->integer('booking_id')->unsigned();
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+            $table->integer('seat_id')->unsigned();
+            $table->foreign('seat_id')->references('id')->on('seats')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        // throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
     }
 
     /**
