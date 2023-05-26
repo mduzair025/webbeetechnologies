353925c3cccaf85d2899aa56564a1527edeadc17
mduzair025 <mduzair025@gmail.com>
2023-05-26 17:35:38 +0500

    3rd task has been done
    I have used in 3rd task HasMany and belongsTo on self table

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
-
+    public function children(){
+        return $this->hasMany(MenuItem::class, 'parent_id')->with('children');
+    }
+    
+    public function parents(){
+        return $this->belongsTo(MenuItem::class, 'parent_id')->with('parents');
+    }
 }
